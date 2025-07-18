ac4f1f56c35867702407bc83ae2d07fe97ac15c4
content-cn <87414959+content-cn@users.noreply.github.com>
2025-07-18 19:02:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Enable CORS for all requests
 app.use(cors());
